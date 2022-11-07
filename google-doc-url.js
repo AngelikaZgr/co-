7c30aa32ce8ad2953590a57d8819516dcbe159cb
AngelikaZgr <117667339+AngelikaZgr@users.noreply.github.com>
2022-11-07 18:02:13 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSZN-7Qynbolt-vnZDgI6gkhCkOG-PBWX-R2-do2KGHB8zQP023FQA3EfHDMRgUMMuDQxcetZhex07_/pubhtml#';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Cvmpfrn7JVsoBbVV7FjqT_v3ZHNuFUHZgfz60UB1gWI/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
